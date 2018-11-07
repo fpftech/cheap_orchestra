5640d7a440a184ef0a08cb97bee6c86ef7533f53
André Portela <andre_portela_@hotmail.com>
2018-11-07 17:31:43 -0400

    added Fabric as dependency to handle ssh

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'python-slugify>=1.2.6', 'PyYAML>=3.13']
+requirements = ['Click>=6.0', 'python-slugify>=1.2.6', 'PyYAML>=3.13', 'Fabric>=2.4.0']
 
 setup_requirements = []
 
